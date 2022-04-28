5e3f5d14d6c2e3ffb8dba08a512d345368f92218
Meena A <Meena.A@krones.in>
2022-04-28 10:45:39 +0530

    Removed Unused Namespaces for tes

--- ToDoApplication/ViewModels/ToDoItemTagsViewModel.cs
+++ ToDoApplication/ViewModels/ToDoItemTagsViewModel.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
-using ToDoApplication.Command;
 using ToDoApplication.Model;
 using ToDoApplication.Repositories;
 
 namespace ToDoApplication.ViewModels
 {
-	internal class ToDoItemTagsViewModel
+    internal class ToDoItemTagsViewModel
 	{
 		private ITagRepository _tagRepository;
 
