c8d4070478faaf559051b0d0bcbac96fdcf3eae3
Jagadeesh HM <Jagadeesh.HM@krones.in>
2022-05-02 10:39:57 +0530

    added validation foir name in toDoItem tags

---
+++ ToDoApplication/Validation/Rules/NotEmptyValidationRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToDoApplication.Validation.Rules
+{
+	internal class NotEmptyValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			if(value is string str)
+			{
+				if(!string.IsNullOrEmpty(str))
+				{
+					return ValidationResult.ValidResult;
+				}else
+				{
+					return new ValidationResult(false, "This field cannot be empty or white space and Value should be string");
+				}
+			}
+			else
+			{
+				return new ValidationResult(false, "Value should be string");
+			}
+		}
+	}
+}
--- ToDoApplication/ViewModels/ToDoItemTagsViewModel.cs
+++ ToDoApplication/ViewModels/ToDoItemTagsViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Media;
 using ToDoApplication.Model;
 using ToDoApplication.Repositories;
 
 namespace ToDoApplication.ViewModels
 {
-    internal class ToDoItemTagsViewModel
+    internal class ToDoItemTagsViewModel : ValidationViewModelBase
 	{
 		private ITagRepository _tagRepository;
-
 		public Guid Id { get; set; }
 		//public string Name { get; set; }
 
@@ -36,11 +37,42 @@ namespace ToDoApplication.ViewModels
 			set
 			{ 
 				_name = value;
+				if(ValidateName())
+				{ 
 				_tagRepository.Update(createModel());
-			
+				}
 			}
 		}
 
+		private bool ValidateName()
+		{
+			//validate that name is not empty
+			if (string.IsNullOrEmpty(Name))
+			{
+				SetError(nameof(Name), "Tag Cannot be empty");
+				return false;
+			}
+			//validate that name is unique
+			else if(NameIsNotUnique())
+			{
+				SetError(nameof(Name), "Tagname Must be unique");
+				return false;
+			}else
+			{
+				Reseterror(nameof(Name));
+				return true;
+			}
+				
+		}
+
+		private bool NameIsNotUnique()
+		{
+			var otherTagNames = _tagRepository.GetAll()
+						   .Where(tag => tag.Id != this.Id)
+						   .Select(tag => tag.Name);
+			return otherTagNames.Contains(Name);           
+		   
+		}
 
 		public  ToDoItemTagsViewModel (ToDoItemTags tags,ITagRepository tagRepository)
 		{
